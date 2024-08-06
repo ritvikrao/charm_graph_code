0fa86bda72e1a8f0b9a3fb3769e1c4102e99bc01
Ritvik Rao <rrao@login03.frontier.olcf.ornl.gov>
2024-08-06 19:14:12 -0400

    batch script change

--- weighted_htram_smp_frontier.sh
+++ weighted_htram_smp_frontier.sh
@@ -15,4 +15,4 @@ export PPN=6
 # single node: --network=single_node_vni
 # srun --network=single_node_vni ./weighted_htram_smp 62500000 /lustre/orion/bip249/scratch/rrao/1B.csv 100 1 +ppn 6 +setcpuaffinity #+traceroot weighted_htram_Smp_projections
 # srun --network=single_node_vni ./weighted_htram_smp 20000000 320000000 100 1 1 +ppn 6 +setcpuaffinity #+traceroot weighted_htram_Smp_projections
-srun ./weighted_htram_smp 62500000 1000000000 100 1 1 0.999 0.05 +ppn $PPN +setcpuaffinity
+srun ./weighted_htram_smp 62500000 /lustre/orion/bip249/scratch/rrao/1B.csv 100 1 0 0.999 0.05 +ppn $PPN +setcpuaffinity #+traceroot weighted_htram_Smp_projections
